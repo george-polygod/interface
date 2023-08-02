ef8524f7006da077e17c54bea08eae864ec77524
george-polygod <george@polygod.io>
2023-08-02 16:10:26 +0300

    updated

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
-export const UNI_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
+export const UNI_LIST = 'https://ipfs.filebase.io/ipfs/QmS2NTS4Btty4812M3vUSZU8i2v8qPVwemp9vskh3Gygv5'
 export const UNI_EXTENDED_LIST = 'https://gateway.ipfs.io/ipns/extendedtokens.uniswap.org'
-const UNI_UNSUPPORTED_LIST = 'https://ipfs.filebase.io/ipfs/Qmag7V6VzUQxN7Qfzr8zEyGkuV1JhDqkYTAd9efBR9MTj9'
+const UNI_UNSUPPORTED_LIST = 'https://ipfs.filebase.io/ipfs/Qme6X3w6H8Xrh7DCtsApad61CejNQGtiJ8pSdmcuZgEoRv'
 const AAVE_LIST = 'tokenlist.aave.eth'
 const BA_LIST = 'https://raw.githubusercontent.com/The-Blockchain-Association/sec-notice-list/master/ba-sec-list.json'
 // TODO(WEB-2282): Re-enable CMC list once we have a better solution for handling large lists.
